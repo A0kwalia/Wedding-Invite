e8e240f165368865f989afce174a5364926028e0
A0kwalia <dev.alvinkwalia@gmail.com>
2024-12-23 11:56:31 +0300

    Refactor: simplify HTML structure and streamline styling using utility classes and CSS variables

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Set the countdown target date and time (YYYY-MM-DDTHH:MM:SS format)
-const targetDate = new Date("2024-12-31T23:59:59").getTime();
+const targetDate = new Date("2025-04-27T15:59:59").getTime();
 
 function updateCountdown() {
     const now = new Date().getTime();
